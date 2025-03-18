86a6312a868aac711c31e73b02c2d43c755c39ec
spacee <ecren.talasli@gmail.com>
2025-03-18 18:39:04 +0000

    Adjust codebase before deployment to ec2

---
+++ server/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [{
+    name: 'free-time-finder',
+    script: 'index.js',
+    env: {
+      NODE_ENV: 'development',
+      ENV_VAR1: 'value'
+    }
+  }],
+}
--- server/index.js
+++ server/index.js
@@ -75,6 +75,7 @@ app.use((req, res) => {
 });
 
 if (require.main === module) {
+  const port = Number(process.env.PORT) || 3000;
   app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`);
   });
